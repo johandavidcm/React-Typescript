b0ca41832272cafdc658dcb51ff37b88fcd2e9ce
Johan David <johancarras16@gmail.com>
2021-03-13 01:18:10 -0500

    Pendiente ultimo video

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ContadorRed } from "./components/ContadorRed"
 import { Counter } from "./components/Counter"
 import { TimerPadre } from "./components/TimerPadre"
 import { Usuario } from "./components/Usuario"
@@ -12,6 +13,9 @@ const App = () => {
       <h2>useEffet</h2>
       <hr/>
       <TimerPadre/>
+      <h2>Contador Red</h2>
+      <hr/>
+      <ContadorRed/>
     </>
   );
 }
